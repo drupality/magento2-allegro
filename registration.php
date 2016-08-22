e69bf0d2853beed198e6a306aeec378f337bdfe3
marek.kisiel <marek.kisiel@unic.com>
2016-08-22 14:27:37 +0200

    init commit

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marek.kisiel
+ * Date: 22/08/16
+ * Time: 13:51
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Drupality_Allegro', __DIR__);
